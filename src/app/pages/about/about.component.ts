39cbe2048f125d159256ccf493432fc86a114fff
William Applegate <wikapple@gmail.com>
2025-06-29 01:34:54 -0400

    about me changes

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -4,6 +4,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ImageDialogComponent } from '../../shared/image-dialog/image-dialog.component';
 
 @Component({
   selector: 'app-about',
@@ -18,5 +20,13 @@ import { RouterModule } from '@angular/router';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  constructor(private dialog: MatDialog) {}
 
+    openImage(imgUrl: string): void {
+    this.dialog.open(ImageDialogComponent, {
+      data: { imgUrl },
+      panelClass: 'custom-dialog',
+      maxWidth: '90vw',
+    });
+  }
 }
